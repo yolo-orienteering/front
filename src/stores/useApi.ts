b5546dc8c2879aa59451fa757ec78c2c6fa2029f
Michael Scheurer <michael-scheurer@int.seccom.ch>
2025-03-23 21:36:50 +0100

    add default prod api url

--- src/stores/useApi.ts
+++ src/stores/useApi.ts
@@ -4,7 +4,7 @@ import type { CustomDirectusTypes } from '../types/DirectusTypes'
 
 export const useApi = defineStore('useApi', () => {
   const API_URL = (): string => {
-    return import.meta.env.VITE_API_URL || ''
+    return import.meta.env.VITE_API_URL || 'https://cic01at5ntdhf7wg13631.cleavr.xyz/'
   }
 
   const directus = createDirectus<CustomDirectusTypes>(API_URL())
