159491bd1af3637dea9493eb5e3dcd5938e62694
Michael Scheurer <michael-scheurer@int.seccom.ch>
2025-03-27 15:43:23 +0100

    add myraces

---
+++ src/pages/races/MyRaces.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row justify-between items-center">
+    <div class="text-h5 q-pt-md">
+      Meine Läufe
+    </div>
+
+    <div v-if="syncCenter.myRacesSorted.length" class="col-12">
+      <race-timeline :races="syncCenter.myRacesSorted" :show-load-more="false" />
+    </div>
+
+    <div v-else class="col-12 q-pt-md">
+      <q-banner dark>
+        Du hast dir noch keine Läufe vorgemerkt. Stelle Dir Deine Liste zusammen.
+        <template #avatar>
+          <q-icon name="info" size="md" class="q-pr-sm" />
+        </template>
+        <template #action>
+          <q-btn href="/">
+            Jetzt Läufe entdecken
+          </q-btn>
+        </template>
+      </q-banner>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, nextTick } from 'vue'
+import RaceTimeline from 'components/races/RaceTimeline.vue'
+import { useSyncCenter } from 'src/stores/syncCenter'
+import { useEventBus } from 'src/stores/useEventBus'
+
+const syncCenter = useSyncCenter()
+const { eventBus } = useEventBus()
+
+const notify = ref<boolean>(true)
+
+onMounted(async () => {
+  await nextTick(() => {
+    eventBus.emit('scrollToSavedPosition')
+  })
+})
+
+</script>
