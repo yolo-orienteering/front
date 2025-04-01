37fa6b577194f0e01d14f837f648ca9792e2c766
Michael Scheurer <michael-scheurer@int.seccom.ch>
2025-04-01 20:18:51 +0200

    add automatic departure time

---
+++ src/composables/useMyDepartures.ts
@@ -0,0 +1,101 @@
+import { readItems } from '@directus/sdk'
+import { useQuasar } from 'quasar'
+import { useSyncCenter } from 'src/stores/syncCenter'
+import { useApi } from 'src/stores/useApi'
+import { Race, RaceCategory, UserDeparture } from 'src/types/DirectusTypes'
+import { onMounted, ref, watch } from 'vue'
+
+export function useMyDepartures () {
+  const {directus} = useApi()
+  const syncCenter = useSyncCenter()
+
+  const myDepartures = ref<UserDeparture[]>([])
+
+  // local storage
+  const localStorage = useQuasar().localStorage
+  const MY_DEPARTURES_KEY = 'my-departures'
+
+  onMounted(async () => {
+    // 1. read data from store
+    readMyDeparturesFromStore()
+    // 2. try to update from remote
+    await updateMyDeparturesFromDirectus()
+  })
+
+  // update departures whenever user object changes
+  // todo: only every 3 seconds to avoid on every keyboard stroke
+  watch(
+    () => syncCenter.userIdentifier,
+    async () => {
+      await updateMyDeparturesFromDirectus()
+    },
+    {
+      deep: true
+    }
+  )
+
+  // update local store
+  watch(
+    () => myDepartures,
+    () => localStorage.set(MY_DEPARTURES_KEY, myDepartures.value),
+    {deep: true}
+  )
+
+  function readMyDeparturesFromStore (): void {
+    const myDeparturesFromStore = localStorage.getItem<UserDeparture[]>(MY_DEPARTURES_KEY)
+    if (myDeparturesFromStore) {
+      myDepartures.value = myDeparturesFromStore
+    }
+  }
+
+  async function updateMyDeparturesFromDirectus (): Promise<void> {
+    try {
+      if (!syncCenter.userIdentifier) {
+        return
+      }
+
+      myDepartures.value = await directus.request<UserDeparture[]>(readItems('UserDeparture', {
+        filter: {
+          user: {
+            composedIdentifierSolv: {
+              _eq: syncCenter.userIdentifier
+            }
+          }
+        },
+        fields: ['id', 'race', 'startTimeInMinutes', {
+          raceCategory: [
+            'id', 'name', 'amountOfControls', 'distanceInMeter', 'equidistanceInMeter'
+          ]
+        }]
+      }))
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  function getFormatedDeparture(raceId: string): string | undefined {
+    const departure = getDepartureFor(raceId)
+    if (!departure) {
+      return
+    }
+  
+    const startTimeInMinutes = departure.startTimeInMinutes
+    if (!startTimeInMinutes) {
+      return
+    }
+  
+    const hours = Math.floor(startTimeInMinutes / 60).toString().padStart(2, '0')
+    const minutes = (startTimeInMinutes % 60).toString().padStart(2, '0')
+    return `${hours}:${minutes}, ${(departure.raceCategory as RaceCategory).name}`
+  }
+
+  function getDepartureFor (raceId: string | null | undefined): UserDeparture | undefined {
+    return myDepartures.value.find(departure => departure.race === raceId)
+  }
+
+  return {
+    myDepartures,
+    getDepartureFor,
+    getFormatedDeparture
+  }
+}
