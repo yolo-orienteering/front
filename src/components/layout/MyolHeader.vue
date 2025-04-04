530146ec6075bace4f9b60e3bb321b998a66fa6c
Michael Scheurer <michael-scheurer@int.seccom.ch>
2025-04-04 21:43:09 +0200

    add favicon

--- src/components/layout/MyolHeader.vue
+++ src/components/layout/MyolHeader.vue
@@ -1,8 +1,33 @@
 <template>
   <q-header class="text-primary bg-white" elevated reveal :reveal-offset="5">
-    <div class="col-12 text-h4 text-weight-bold text-center q-my-sm">o-mate</div>
+    <div class="row items-center q-ma-sm">
+      <div class="col-auto">
+        <img src="/img/logo.png" style="height: 65px; width: auto; display: block; margin: auto;" />
+      </div>
+      <div class="col-auto q-pl-md">
+        <h3 class="title">o-mate</h3>
+        <!-- <p class="claim">ORIENTEERING</p> -->
+      </div>
+    </div>
   </q-header>
 </template>
 
 <script setup lang="ts">
 </script>
+
+<style lang="scss">
+.title {
+  margin: 0;
+  font-weight: 300;
+  font-size: 48px;
+  line-height: 0.95;
+}
+
+.claim {
+  margin-left: 45px;
+  margin-bottom: 0;
+  font-size: 15px;
+  color: $secondary;
+  font-weight: 400;
+}
+</style>
